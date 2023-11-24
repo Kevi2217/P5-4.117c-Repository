dec778bc8a0b9da688f3890cd833d1c06a91bdd6
Freddy D <93995722+HunterFreddy69@users.noreply.github.com>
2023-11-24 17:24:37 +0100

    Add files via upload

--- main_kode/main_start.R
+++ main_kode/main_start.R
@@ -25,6 +25,8 @@ test_data <- homedata %>%
   dplyr::group_by(kommunenavn) %>%
   dplyr::sample_n(size = (n() / 10), replace = FALSE) %>%
   dplyr::group_split()
+# Fjerner kommunenavn søjlen for alle dataframes i listen
+test_data <- lapply(test_data, function(df) df[, !names(df) %in% "kommunenavn", drop = FALSE])
 
 
 # Fjerner test_data fra homedata og laver training_data 
@@ -32,12 +34,15 @@ training_data <- homedata %>%
   dplyr::anti_join(bind_rows(test_data)) %>%
   dplyr::group_by(kommunenavn) %>%
   dplyr::group_split()
+# Fjerner kommunenavn søjlen for alle dataframes i listen
+training_data <- lapply(training_data, function(df) df[, !names(df) %in% "kommunenavn", drop = FALSE])
 
 ######################################### ALT ANALYSE HERUNDER ###########################################
 # test data og training_data er inddelt i en liste med længde 3.
 # 1 er Aalborg,
 # 2 er Aarhus,
 # 3 er København.
+
 
 
 
