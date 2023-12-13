b90cc0564bbde2aa0601f9eae1ae7f2494619f23
Freddy D <93995722+HunterFreddy69@users.noreply.github.com>
2023-12-13 09:40:10 +0100

    Add files via upload

--- main_kode/main_start.R
+++ main_kode/main_start.R
@@ -12,6 +12,7 @@ library(RColorBrewer)
 library(ggcorrplot)
 library(MASS)
 library(stringr)
+library(car)
 
 # Definere generelle variable
 set.seed(69)
@@ -158,7 +159,7 @@ par(mfrow = c(1, 1))
 
 # Fjerner outliers og plotter igen
 
-training_data[[2]] <- training_data[[2]] %>%
+aar_uden_outlier <- training_data[[2]] %>%
   #beloeb_ejerudgift
   dplyr::filter(beloeb_ejerudgift < 500000) %>%
   #ejd_opfoerelsesaar
@@ -167,20 +168,20 @@ training_data[[2]] <- training_data[[2]] %>%
   dplyr::filter(dist_skole < 2)
 
 par(mfrow = c(1, 3))
-plot(training_data[[2]]$beloeb_ejerudgift,
-     training_data[[2]]$pris_salg / 1000000,
+plot(aar_uden_outlier$beloeb_ejerudgift,
+     aar_uden_outlier$pris_salg / 1000000,
      main = "Price vs Cost of Ownership",
      xlab = "Cost of Ownership",
      ylab = "Price (in millions)")
 
-plot(training_data[[2]]$ejd_opfoerelsesaar,
-     training_data[[2]]$pris_salg / 1000000,
+plot(aar_uden_outlier$ejd_opfoerelsesaar,
+     aar_uden_outlier$pris_salg / 1000000,
      main = "Price vs Year of Construction",
      xlab = "Year of Construction",
      ylab = "Price (in millions)")
 
-plot(training_data[[2]]$dist_skole,
-     training_data[[2]]$pris_salg / 1000000,
+plot(aar_uden_outlier$dist_skole,
+     aar_uden_outlier$pris_salg / 1000000,
      main = "Price vs School",
      xlab = "Distance to Nearest School",
      ylab = "Price (in millions)")
@@ -237,28 +238,28 @@ plot(training_data[[1]]$ejd_energimaerke,
 
 par(mfrow = c(1, 1))
 
-training_data[[1]] <- training_data[[1]] %>%
-  dplyr::filter(ejd_energimaerke != "F")
-
-training_data[[1]]$ejd_energimaerke <- droplevels(training_data[[1]]$ejd_energimaerke)
-
-par(mfrow = c(1, 2))
-
-plot(training_data[[1]]$bynavn,
-     training_data[[1]]$pris_salg / 1000000,
-     main = "Price vs Town",
-     ylab = "Price (in millions)",
-     xlab = "",
-     las = 2,
-     cex.axis = 0.7)
-
-plot(training_data[[1]]$ejd_energimaerke,
-     training_data[[1]]$pris_salg / 1000000,
-     main = "Price vs Energy Label",
-     xlab = "Energy label",
-     ylab = "Price (in millions)")
-
-par(mfrow = c(1, 1))
+# training_data[[1]] <- training_data[[1]] %>%
+#   dplyr::filter(ejd_energimaerke != "F")
+# 
+# training_data[[1]]$ejd_energimaerke <- droplevels(training_data[[1]]$ejd_energimaerke)
+# 
+# par(mfrow = c(1, 2))
+# 
+# plot(training_data[[1]]$bynavn,
+#      training_data[[1]]$pris_salg / 1000000,
+#      main = "Price vs Town",
+#      ylab = "Price (in millions)",
+#      xlab = "",
+#      las = 2,
+#      cex.axis = 0.7)
+# 
+# plot(training_data[[1]]$ejd_energimaerke,
+#      training_data[[1]]$pris_salg / 1000000,
+#      main = "Price vs Energy Label",
+#      xlab = "Energy label",
+#      ylab = "Price (in millions)")
+# 
+# par(mfrow = c(1, 1))
 # AAR (bynavn, energimaerke, ejd_altan)
 # Fjerner ejd_altan
 par(mfrow = c(1, 3))
@@ -337,49 +338,135 @@ model_kbh_2 <- lm(pris_salg ~ bynavn + antalfremvisninger + adresse_etage +
 # names(coef(model_kbh_2))
 
 
-############################ LOG MODEL 2 ####################################
-model_aal_2_log <- lm(log(pris_salg) ~ bynavn + antalfremvisninger + areal_bolig +
+
+
+
+
+
+###################### MODEL 3 (FJERN OUTLIERS OG LOG-TRANSFORMER) ###########################
+# Fjerner outliers
+#Definere data listen uden outliers
+training_data_wo_outlier <- c()
+
+training_data_wo_outlier[[1]] <- training_data[[1]][
+  -which(as.vector(ifelse(is.nan(cooks.distance(model_aal_1s)),
+                          0,
+                          cooks.distance(model_aal_1s))) > 4 / length(cooks.distance(model_aal_1s))), ]
+
+training_data_wo_outlier[[2]] <- training_data[[2]][
+  -which(as.vector(ifelse(is.nan(cooks.distance(model_aar_1s)),
+                          0,
+                          cooks.distance(model_aar_1s))) > 4 / length(cooks.distance(model_aar_1s))), ]
+training_data_wo_outlier[[3]] <- training_data[[3]][
+  -which(as.vector(ifelse(is.nan(cooks.distance(model_kbh_1s)),
+                          0,
+                          cooks.distance(model_kbh_1s))) > 4 / length(cooks.distance(model_kbh_1s))), ]
+
+# Laver modeller uden outliers fra model 1s
+model_aal_3_1 <- lm(pris_salg ~ bynavn + beloeb_ejerudgift + antalfremvisninger + areal_bolig +
                     ejd_antalplan + ejd_energimaerke + ejd_ombygningsaar +
-                    dist_skole + dist_raadhus, data = training_data[[1]])
-# summary(model_aal_2_log)
-# names(coef(model_aal_2_log))
+                    dist_skole + dist_raadhus,
+                    data = training_data_wo_outlier[[1]])
+# summary(model_aal_3_1)
 
-model_aar_2_log <- lm(log(pris_salg) ~ bynavn + areal_bolig + ejd_antalplan +
-                    ejd_energimaerke + dist_raadhus, data = training_data[[2]])
-# summary(model_aar_2_log)
-# names(coef(model_aar_2_log))
+model_aar_3_1 <- lm(pris_salg ~ bynavn + beloeb_ejerudgift + areal_bolig + ejd_altan +
+                      ejd_antalplan + ejd_energimaerke + ejd_opfoerelsesaar +
+                      dist_skole + dist_raadhus,
+                    data = training_data_wo_outlier[[2]])
+# summary(model_aar_3_1)
 
+model_kbh_3_1 <- lm(pris_salg ~ bynavn + beloeb_ejerudgift + antalfremvisninger +
+                      adresse_etage + areal_bolig + ejd_altan + ejd_antalrum +
+                      ejd_energimaerke + ejd_opfoerelsesaar + dist_raadhus,
+                    data = training_data_wo_outlier[[3]])
+# summary(model_kbh_3_1)
 
-model_kbh_2_log <- lm(log(pris_salg) ~ bynavn + antalfremvisninger + adresse_etage +
-                    areal_bolig + ejd_energimaerke + ejd_opfoerelsesaar, data = training_data[[3]])
-# summary(model_kbh_2_log)
-# names(coef(model_kbh_2)_log)
+## LOG-TRANSFORM AF MODEL 3##
+model_aal_3_2 <- lm(log(pris_salg) ~ bynavn + beloeb_ejerudgift + antalfremvisninger + areal_bolig +
+                      ejd_antalplan + ejd_energimaerke + ejd_ombygningsaar +
+                      dist_skole + dist_raadhus,
+                    data = training_data_wo_outlier[[1]])
+# summary(model_aal_3_2)
 
+model_aar_3_2 <- lm(log(pris_salg) ~ bynavn + beloeb_ejerudgift + areal_bolig + ejd_altan +
+                      ejd_antalplan + ejd_energimaerke + ejd_opfoerelsesaar +
+                      dist_skole + dist_raadhus,
+                    data = training_data_wo_outlier[[2]])
+# summary(model_aar_3_2)
 
-par(mfrow = c(2, 3))
-plot(model_aal_2, main = "AAL", which = 1)
-plot(model_aar_2, main = "AAR", which = 1)
-plot(model_kbh_2, main = "CPH", which = 1)
+model_kbh_3_2 <- lm(log(pris_salg) ~ bynavn + beloeb_ejerudgift + antalfremvisninger +
+                      adresse_etage + areal_bolig + ejd_altan + ejd_antalrum +
+                      ejd_energimaerke + ejd_opfoerelsesaar + dist_raadhus,
+                    data = training_data_wo_outlier[[3]])
+# summary(model_kbh_3_2)
 
-plot(model_aal_2_log, main = "AAL", which = 1)
-plot(model_aar_2_log, main = "AAR", which = 1)
-plot(model_kbh_2_log, main = "CPH", which = 1)
+# res/fitted normal 1s
+par(mfrow = c(1, 3))
+plot(model_aal_1s, main = "AAL", which = 1)
+plot(model_aar_1s, main = "AAR", which = 1)
+plot(model_kbh_1s, main = "KBH", which = 1)
 par(mfrow = c(1, 1))
 
-par(mfrow = c(2, 3))
-plot(model_aal_2, main = "AAL", which = 2)
-plot(model_aar_2, main = "AAR", which = 2)
-plot(model_kbh_2, main = "CPH", which = 2)
+# res/fitted 1s UDEN outliers
+par(mfrow = c(1, 3))
+plot(model_aal_3_1, main = "AAL", which = 1)
+plot(model_aar_3_1, main = "AAR", which = 1)
+plot(model_kbh_3_1, main = "KBH", which = 1)
+par(mfrow = c(1, 1))
 
-plot(model_aal_2_log, main = "AAL", which = 2)
-plot(model_aar_2_log, main = "AAR", which = 2)
-plot(model_kbh_2_log, main = "CPH", which = 2)
+# res/fitted 1s UDEN outliers OG efter LOG
+par(mfrow = c(1, 3))
+plot(model_aal_3_2, main = "AAL", which = 1)
+plot(model_aar_3_2, main = "AAR", which = 1)
+plot(model_kbh_3_2, main = "KBH", which = 1)
+par(mfrow = c(1, 1))
+
+
+# QQ plots
+# qq 1s UDEN outliers
+par(mfrow = c(1, 3))
+plot(model_aal_3_1, main = "AAL", which = 2)
+plot(model_aar_3_1, main = "AAR", which = 2)
+plot(model_kbh_3_1, main = "KBH", which = 2)
+par(mfrow = c(1, 1))
+
+# qq 1s UDEN outliers OG efter LOG
+par(mfrow = c(1, 3))
+plot(model_aal_3_2, main = "AAL", which = 2)
+plot(model_aar_3_2, main = "AAR", which = 2)
+plot(model_kbh_3_2, main = "KBH", which = 2)
 par(mfrow = c(1, 1))
 
 
 
 
-# confint(model_aar_2, level = 0.95)
+# plot(training_data_wo_outlier[[1]]$beloeb_ejerudgift,
+#      training_data_wo_outlier[[1]]$pris_salg / 1000000,
+#      main = "Price vs Town",
+#      ylab = "Price (in millions)",
+#      xlab = "",
+#      las = 2,
+#      cex.axis = 0.7)
+
+
+
+
+              ## QQ plots ##
+# par(mfrow = c(2, 3))
+# plot(model_aal_2, main = "AAL", which = 5)
+# plot(model_aar_2, main = "AAR", which = 1)
+
+
+# ggplot(data.frame(fitted_values = fitted(model_aal_2),
+#   standardized_residuals = residuals(model_aal_2) / sd(residuals(model_aal_2))), aes(x = fitted(model_aal_2), y = rstandard(model_aal_2))) +
+#   geom_point(color = "black", size = 2, shape = 1) +  # Adjust point color and size as needed
+#   geom_hline(yintercept = 0, linetype = "dotted", color = "azure3") +  # Dotted line at y = 0
+#   labs(title = "My Model", x = "Fitted Values", y = "Standardized Residuals") +
+#   ylim(-5, 5) +
+#   xlim(0, 6000000) +
+#   theme_minimal()
+
+
 
 
 
